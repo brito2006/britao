ac78462c41b548286bc3538d03c9fec79ae28060
brito2006 <141193780+brito2006@users.noreply.github.com>
2024-10-18 21:49:56 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,33 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Carro',
+    'Qual o carro mais rapido?',
+    'SSC Tuatara.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital da Inglaterra?',
+    'A capital da Inglaterra é Londres'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Geografia',
+    'Qual o pais mais pobre?',
+    'E o Sudão do Sul  '
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz Tchau brigado em Inglês?',
+    'Tchau brigado em ingles é Bye Thanks'
+)
+criaCartao(
+    'Sexologia',
+    'O que acontece se gozar dentro?',
+    'a mulher engravida'
+)
+criaCartao(
+    'Musica',
+    'Qual dj que so lamça pedrada?',
+    'DJ Vittin'
 )
